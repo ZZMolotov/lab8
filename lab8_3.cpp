80154dbc2b4670b08614190898ebb74844ef928b
ZZMolotov <zzmolotov8559@gmail.com>
2025-12-16 15:47:02 +0700

    lab8_3 fixed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,47 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string Character;
+    long long age, height, bounty;
+    cout<< "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout<< "Enter your height: ";
+        cin >> height;
+        if (height < 100){
+            Character = "Chopper";
+            cout << "Your character = "<< Character;
+        }else if (height < 180){
+            Character = "Usopp";
+            cout << "Your character = "<< Character;
+        }else if (height >= 180){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty <= 1100000000){
+                Character = "Sanji";
+                cout << "Your character = "<< Character;
+            }else if (bounty > 1100000000){
+                Character = "Zoro";
+                cout << "Your character = "<< Character;
+            }
+        }
+    }else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty <= 500000000){
+            Character = "Franky";
+            cout << "Your character = "<< Character;
+        }else if (bounty > 500000000){
+            Character = "Jinbe";
+            cout << "Your character = "<< Character;
+        }
+    }else{
+        Character = "Brook";
+        cout << "Your character = "<< Character;
+    }
+}
